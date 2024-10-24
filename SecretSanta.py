d9d7ba316e03dc0a6a89320faf0bd39ce1ea7416
Antonio Bitonti <antoniobitonti@live.it>
2024-10-24 18:47:58 +0200

    modifiche + aggiunta HTML

--- SecretSanta.py
+++ SecretSanta.py
@@ -1,19 +1,8 @@
 import random
 import pandas as pd
-
+from send_mail import send_mail
 df= pd.read_csv('Jakala_Secret_Santa.csv', sep=',')
-associazioni_forzate=[['rosanna.pannozzo@jakala.com',	'rocco.oliva@jakala.com'],
-    ['rocco.oliva@jakala.com',	'rosanna.pannozzo@jakala.com'],
-    ['antonella.vassallo@jakala.com',	'davide.ciliberto@jakala.com'],
-    ['davide.ciliberto@jakala.com',	'antonella.vassallo@jakala.com'],
-    ['daniele.mauro@jakala.com',	'andrea.vitali@jakala.com'],
-    ['andrea.vitali@jakala.com',	'daniele.mauro@jakala.com'],
-    ['ezio.dellirocili@jakala.com',	'gennaro.oriolo@jakala.com'],
-    ['gennaro.oriolo@jakala.com',	'ezio.dellirocili@jakala.com'],
-    ['irene.cutrona@jakala.com',	'gennaro.oriolo@jakala.com'],
-    ['gennaro.oriolo@jakala.com',	'irene.cutrona@jakala.com']]
 
-df = df[~df['Email'].isin([i[0] for i in associazioni_forzate])]
 l=df['Email'].tolist()
 
 l2 = l.copy()
@@ -23,11 +12,6 @@ while aux:
     for i in range(len(l)):
         if l[i]==l2[i]: break
         elif i==len(l)-1: aux=False
-#f= open("SecretSantaAssociazioni.csv",'w')
-# f.write(f'"regala","riceve","regala_nome","regala_cognome","riceve_nome","riceve_cognome"\n')
-# for i in range(len(l)):
-#     f.write(f'"{l[i]}","{l2[i]}"\n')  
-# f.close()
 l3=[]
 
 for i in range(len(l)):
@@ -36,16 +20,16 @@ for i in range(len(l)):
         regala_cognome = l[i].split('.')[1].split('@')[0].capitalize()
         riceve_nome = l2[i].split('.')[0].capitalize()
         riceve_cognome = l2[i].split('.')[1].split('@')[0].capitalize()
-        messaggio = f"Ciao {regala_nome},\nabbiamo appena terminato il sorteggio per il Secret Santa🎅siamo contenti che tu abbia deciso di partecipare!\nDovrai fare un regalo a {riceve_nome} {riceve_cognome}, cerca di rispettare il budget da 1-10€ il pensiero è quello che basta 😉\nBuon divertimento!"
-        l3.append([l[i],l2[i],regala_nome,regala_cognome,riceve_nome,riceve_cognome,messaggio])
-for l in associazioni_forzate:
-    regala_nome = l[0].split('.')[0].capitalize()
-    regala_cognome = l[0].split('.')[1].split('@')[0].capitalize()
-    riceve_nome = l[1].split('.')[0].capitalize()
-    riceve_cognome = l[1].split('.')[1].split('@')[0].capitalize()
-    messaggio = f"Ciao {regala_nome},\nabbiamo appena terminato il sorteggio per il Secret Santa🎅!\nDovrai fare un regalo a {riceve_nome} {riceve_cognome}, cerca di rispettare il budget da 1-10€ il pensiero è quello che basta 😉\nBuon divertimento!"
-    l3.append([l[0],l[1],regala_nome,regala_cognome,riceve_nome,riceve_cognome,messaggio])
+        #messaggio = f"Ciao {regala_nome},\nabbiamo appena terminato il sorteggio per il Secret Santa🎅siamo contenti che tu abbia deciso di partecipare!\nDovrai fare un regalo a {riceve_nome} {riceve_cognome}, cerca di rispettare il budget da 1-10€ il pensiero è quello che basta 😉\nBuon divertimento!"
+        l3.append({
+            "regala_mail":l[i],
+            "riceve_mail":l2[i],
+            "regala_nome":regala_nome,
+            "regala_cognome":regala_cognome,
+            "riceve_nome":riceve_nome,
+            "riceve_cognome":riceve_cognome
+        })
 
-df= pd.DataFrame(l3, columns=["regala","riceve","regala_nome","regala_cognome","riceve_nome","riceve_cognome","messaggio"])
-print(df)
-df.to_excel('SecretSantaAssociazioni.xlsx')
+
+for i in l3:
+    send_mail(i)
--- send_mail.py
+++ send_mail.py
@@ -12,24 +12,16 @@ from datetime import date, datetime
 import numpy as np
 import google.auth
 
+region_id="your gcp region" #europe-west1 for example
+trigger_id = 'api_trigger/SendMaiSecretSanta' #application integration trigger
 
-DistributionList="antonio.bitonti@jakala.com"
-cc=""
-bcc=""
-object="Pipeline Status"
-PipelineStatus="OK"
-PipelineName="Test"
-Message="Nice job your pipeline is great"
-BUCKET_MAIL_LIST = f'brico_io_dev'
-MAIL_LIST_PATH = 'MailList.json'
 
-def send_email_new(category: str, body_message: str, object: str, gcs_client: callable, bucket_name_mail_list: str, path_mail_list: str , status: str) -> None:
+def send_email(gifter_mail, gifter_name, gifted_name) -> None:
     """
         Send email with alerts using Application Integration.
 
         Args:
             PROJECT_ID (str): The GCP project ID.
-            body_message (str): The email body content.
             subject_message (str): The email subject.
             gcs_client (callable): The GCS client object.
             bucket_name_mail_list (str): The GCS bucket name containing the email list.
@@ -46,45 +38,31 @@ def send_email_new(category: str, body_message: str, object: str, gcs_client: ca
     GCP_CREDENTIALS, PROJECT_ID = google.auth.default()
     auth_req = google.auth.transport.requests.Request()
     GCP_CREDENTIALS.refresh(auth_req)
-    mail_to, cc, bcc = get_mail_list(gcs_client,bucket_name_mail_list, path_mail_list, category)
-    ENV = PROJECT_ID.split('-')[-1]
 
+    mail_to= gifter_mail
+    
+    cc="myBoss@mail.com"
+    bcc="my@mail.com"
     id_access_token = GCP_CREDENTIALS.token
 
     # Definisci l'URL, l'header e i dati
-    url = f"https://integrations.googleapis.com/v1/projects/{PROJECT_ID}/locations/europe-west6/integrations/-:execute"
+    url = f"https://integrations.googleapis.com/v1/projects/{PROJECT_ID}/locations/{region_id}/integrations/-:execute"
     headers = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {id_access_token}"
     }
 
-    if ENV == 'prd' :
-        trigger_id = 'api_trigger/SendMailAlert_API_1'
-        dl=mail_to
-    else:
-        trigger_id = 'api_trigger/SendMailAlert_API_1'
-        dl='antonio.bitonti@jakala.com'
-        cc=""
-        bcc=""
 
-    mail_information = {
-        "PipelineStatus": status,
-        "Message": body_message,
-        "DistributionList": dl,
-        "cc": cc,
-        "bcc": bcc,
-        "object": object,
-        "PipelineName": f"ETL-BRICOIO-{ENV.upper()}"
-    }
+    with open('mail_message.html', 'r', encoding='utf-8') as file:
+        html_body = file.read().replace("@@GIFTER@@",gifted_name).replace("@@GIFTED@@",gifted_name)
     
     data = {
         "trigger_id": trigger_id,
         "inputParameters": {
-                "mail_information":{"jsonValue": json.dumps(mail_information)}
+                "html_body":{"stringValue": html_body}
                 }
             }
 
-    # Invia la richiesta POST
     response = requests.post(url, headers=headers, data=json.dumps(data))
     print(response.text)
     try:
@@ -93,17 +71,5 @@ def send_email_new(category: str, body_message: str, object: str, gcs_client: ca
         print(f"Errore HTTP: {err}")
         raise err
 
-    # Ritorna la risposta
     return None
 
-def get_mail_list(gcs_client,bucket_name, path, category):
-    bucket = gcs_client.get_bucket(bucket_name)
-    blob = gcs.Blob(path, bucket)
-    mail_lists =json.loads(blob.download_as_string())
-    mail_to = mail_lists[category]['mail_to']
-    Cc = mail_lists[category]['cc'] 
-    Bcc = mail_lists[category]['bcc']
-    return mail_to, Cc, Bcc
-
-
-send_email_new("PROVA", Message, object, gcs.Client(), BUCKET_MAIL_LIST, MAIL_LIST_PATH, PipelineStatus)
